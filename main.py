8e553ea6530dbcfeda9f3c4996e27dacdd99e7db
Roman Banakh <banakh.ri@gmail.com>
2019-09-01 21:02:19 +0300

    Initial commit with basic functionality

---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import argparse
+import boto3
+import json
+import yaml
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Create mapping for CloudFormation with AMIs by region',
+                                    epilog='')
+    output_format_group = parser.add_mutually_exclusive_group(required=False)
+    output_format_group.add_argument('-j', '--json', action="store_true")   # make default if it is in ~/.aws/config
+    output_format_group.add_argument('-y', '--yaml', action="store_true")   # make default if it is in ~/.aws/config
+    ami_identifier_group = parser.add_mutually_exclusive_group(required=True)
+    ami_identifier_group.add_argument('-i', '--image-id', action='append')
+    ami_identifier_group.add_argument('-n', '--image-name', action='append')
+    parser.add_argument('-m', '--map-name', action='append', required=True)
+    parser.add_argument('-r', '--region', action='store', )                 # make default if it is in ~/.aws/config
+
+    args = parser.parse_args()
+
+    if args.image_id:
+        if len(args.image_id) != len(args.map_name):
+            parser.error("Number of -i/--image-id should be equal to number of -m/--map-name")
+    elif args.image_name:
+        if len(args.image_name) != len(args.map_name):
+            parser.error("Number of -n/--image-name should be equal to number of -m/--map-name")
+
+    return args
+
+def get_regions(client):
+    return [region['RegionName'] for region in client.describe_regions()['Regions']]
+
+def get_client(resource):
+    return boto3.client(resource)
+
+def get_images_info_by_id(client, images_ids):
+    response = client.describe_images(ImageIds=images_ids)
+    return response['Images']
+
+def get_images_info_by_name(client, images_names):
+    response = client.describe_images(Filters=[
+        {
+            'Name': 'Name',
+            'Values': images_names
+        },
+        {
+            'Name': 'state',
+            'Values': [ 'available' ]
+        }
+    ],)
+    return response['Images']
+
+def parse_images_ids_from_info(images_info):
+    images_ids = [ images['ImageId'] for images in images_info ]
+    return images_ids
+
+def parse_images_names_from_info(images_info):
+    images_names = [ images['Name'] for images in images_info ]
+    return images_names
+
+def main():
+    args = parse_arguments()
+    client = get_client('ec2')
+    aws_regions = get_regions(client)
+    if args.image_id:
+        images_info = get_images_info_by_id(client, args.image_id)
+        images_names = parse_images_names_from_info(images_info)
+    elif args.image_name:
+        images_names = args.image_name[:]
+        print (images_names)
+        
+
+if __name__ == '__main__':
+    main()
