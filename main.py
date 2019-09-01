c623f79454f39a5e36c4073f09332c16ba1c2fce
Roman Banakh <banakh.ri@gmail.com>
2019-09-01 21:44:37 +0300

    Minor changes to parse_arguments and get_client functions

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ def parse_arguments():
     ami_identifier_group = parser.add_mutually_exclusive_group(required=True)
     ami_identifier_group.add_argument('-i', '--image-id', action='append')
     ami_identifier_group.add_argument('-n', '--image-name', action='append')
-    parser.add_argument('-m', '--map-name', action='append', required=True)
-    parser.add_argument('-r', '--region', action='store', )                 # make default if it is in ~/.aws/config
+    parser.add_argument('-k', '--top-level-key', action='append', required=True)
+    parser.add_argument('-r', '--region', action='store', default='us-east-1')                 # make default if it is in ~/.aws/config
 
     args = parser.parse_args()
 
     if args.image_id:
-        if len(args.image_id) != len(args.map_name):
-            parser.error("Number of -i/--image-id should be equal to number of -m/--map-name")
+        if len(args.image_id) != len(args.top_level_key):
+            parser.error("Number of -i/--image-id should be equal to number of -k/--top-level-key")
     elif args.image_name:
-        if len(args.image_name) != len(args.map_name):
-            parser.error("Number of -n/--image-name should be equal to number of -m/--map-name")
+        if len(args.image_name) != len(args.top_level_key):
+            parser.error("Number of -n/--image-name should be equal to number of -k/--top-level-key")
 
     return args
 
 def get_regions(client):
     return [region['RegionName'] for region in client.describe_regions()['Regions']]
 
-def get_client(resource):
-    return boto3.client(resource)
+def get_client(resource, region):
+    return boto3.client(resource, region_name=region)
 
 def get_images_info_by_id(client, images_ids):
     response = client.describe_images(ImageIds=images_ids)
@@ -60,16 +60,22 @@ def parse_images_names_from_info(images_info):
     images_names = [ images['Name'] for images in images_info ]
     return images_names
 
+def generate_map(images_names, top_level_keys, aws_regions):
+    for region in aws_regions:
+        client = get_client('ec2', region)
+        print (region, client)
+
 def main():
     args = parse_arguments()
-    client = get_client('ec2')
+    client = get_client('ec2', args.region)
     aws_regions = get_regions(client)
     if args.image_id:
         images_info = get_images_info_by_id(client, args.image_id)
         images_names = parse_images_names_from_info(images_info)
     elif args.image_name:
         images_names = args.image_name[:]
-        print (images_names)
+    images_map = generate_map(images_names, args.top_level_key, aws_regions)
+    
         
 
 if __name__ == '__main__':
