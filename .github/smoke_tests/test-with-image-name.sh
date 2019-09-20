d877d6a1959a0f5d44d108d067752a6a5a8f4652
Roman Banakh <banakh.ri@gmail.com>
2019-09-20 15:47:06 +0300

    Changed test structure

---
+++ .github/smoke_tests/test-with-image-name.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "------------------ Testing with image names ------------------"
+
+python main.py -n amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2 -k AMILinux -n amzn2-ami-hvm-2.0.20190618-x86_64-gp2 -k AMILinux2 --aws-access-key-id=${AWS_ACCESS_KEY_ID} --aws-secret-access-key=${AWS_SECRET_ACCESS_KEY} -q
+if [ $? == 0 ]; then 
+    echo "[+] Test Passed"
+else
+    echo "[-] Test Failed"
+fi
+
+echo "--------------------------------------------------------------"
