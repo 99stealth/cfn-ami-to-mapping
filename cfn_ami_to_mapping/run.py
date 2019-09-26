6ba75c3e2f6493e5388cae068841c2aaa45d679e
Roman Banakh <banakh.ri@gmail.com>
2019-09-26 12:37:48 +0300

    Error fix

--- cfn_ami_to_mapping/run.py
+++ cfn_ami_to_mapping/run.py
@@ -79,9 +79,9 @@ def main():
         initial_images_map = cfn_ami_to_mapping_enrich.images_info_with_id(full_images_info, initial_images_map_with_image_name)
     images_map = cfn_ami_to_mapping_generate.cfn_ami_mapping_section(initial_images_map, aws_regions, args.map_name, args.quiet, args.aws_access_key_id, args.aws_secret_access_key)
     if args.json:
-        print(Transform.dictionary_to_json(images_map))
+        print(cfn_ami_to_mapping_transform.dictionary_to_json(images_map))
     elif args.yaml:
-        print(Transform.dictionary_to_yaml(images_map))
+        print(cfn_ami_to_mapping_transform.dictionary_to_yaml(images_map))
 
 if __name__ == '__main__':
     try:
--- cfn_ami_to_mapping/transform/transform.py
+++ cfn_ami_to_mapping/transform/transform.py
@@ -2,12 +2,12 @@ import json
 import yaml
 
 class Transform:
-    def dictionary_to_json(images_map):
+    def dictionary_to_json(self, images_map):
         ''' Function receives images map and transforms it ro json '''
         return json.dumps(images_map, indent=4, sort_keys=True)
     
     
-    def dictionary_to_yaml(images_map):
+    def dictionary_to_yaml(self, images_map):
         ''' Function receives images map and transforms it ro yaml '''
         return yaml.dump(images_map)
     
