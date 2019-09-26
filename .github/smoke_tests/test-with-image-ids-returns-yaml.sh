21cfa67f333aac3b58e718bbe7ce6e2fbdcf985e
Roman Banakh <banakh.ri@gmail.com>
2019-09-26 11:45:15 +0300

    Fixed smoke tests

--- .github/smoke_tests/test-with-image-ids-returns-yaml.sh
+++ .github/smoke_tests/test-with-image-ids-returns-yaml.sh
@@ -2,7 +2,7 @@
 
 echo "------------------- Testing with image ids -------------------"
 
-python cfn_ami_to_mapping.run -i ami-035b3c7efe6d061d5 -k AMILinux -i ami-0b898040803850657 -k AMILinux2 --aws-access-key-id=${AWS_ACCESS_KEY_ID} --aws-secret-access-key=${AWS_SECRET_ACCESS_KEY} -q
+python -m cfn_ami_to_mapping.run -i ami-035b3c7efe6d061d5 -k AMILinux -i ami-0b898040803850657 -k AMILinux2 --aws-access-key-id=${AWS_ACCESS_KEY_ID} --aws-secret-access-key=${AWS_SECRET_ACCESS_KEY} -q
 
 if [ $? != 0 ]; then 
     echo "[+] Test Passed"
