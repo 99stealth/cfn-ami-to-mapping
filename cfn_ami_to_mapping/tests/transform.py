4ae3284f393b22a9eb70e3a54b00e5f58994d3b6
Roman Banakh <banakh.ri@gmail.com>
2019-09-26 18:11:56 +0300

    fixing tests

--- cfn_ami_to_mapping/tests/transform.py
+++ cfn_ami_to_mapping/tests/transform.py
@@ -6,12 +6,12 @@ class TestTransform(unittest.TestCase):
         self.transformation = Transform()
 
     def test_dictionary_to_json_with_simple_dictionary(self):
-        test_data = {'1': {'key': 'value'}, '2': {'key': 'value'}}
-        self.assertEquals(self.transformation.dictionary_to_json(test_data), ('{\n    "1": {\n        "key": "value"\n    },\n    "2": {\n        "key": "value"\n    }\n}'))
+        test_data = {'key': 'value'}
+        self.assertEquals(self.transformation.dictionary_to_json(test_data), ('{\n    "key": "value"\n}'))
 
     def test_dictionary_to_yaml_with_simple_dictionary(self):
-        test_data = {'1': {'key': 'value'}, '2': {'key': 'value'}}
-        self.assertEquals(self.transformation.dictionary_to_yaml(test_data), ("'1':\n  key: value\n'2':\n  key: value\n"))
+        test_data = {'key': 'value'}
+        self.assertEquals(self.transformation.dictionary_to_yaml(test_data), ('key: value\n'))
 
 
 if __name__ == '__main__':
