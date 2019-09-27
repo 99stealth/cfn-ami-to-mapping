4f77fe13ee0677e186b5c3ee482535045dd2390b
Roman Banakh <banakh.ri@gmail.com>
2019-09-27 13:46:06 +0300

    Fixed tests

--- cfn_ami_to_mapping/tests/get.py
+++ cfn_ami_to_mapping/tests/get.py
@@ -8,19 +8,19 @@ class TestGet(unittest.TestCase):
 
     def test_images_ids_from_init_id_map_with_one_image(self):
         test_data = {'AMIID': {'image_id': 'ami-035b3c7efe6d061d5'}}
-        self.assertListEquals(self.get.images_ids_from_init_id_map(test_data), ['ami-035b3c7efe6d061d5'])
+        self.assertListEqual(self.get.images_ids_from_init_id_map(test_data), ['ami-035b3c7efe6d061d5'])
 
     def test_images_ids_from_init_id_map_with_two_image(self):
         test_data = {'AMIID': {'image_id': 'ami-035b3c7efe6d061d5'}, 'AMIID2': {'image_id': 'ami-0b898040803850657'}}
-        self.assertListEquals(self.get.images_ids_from_init_id_map(test_data), ['ami-035b3c7efe6d061d5', 'ami-0b898040803850657'])
+        self.assertListEqual(self.get.images_ids_from_init_id_map(test_data), ['ami-035b3c7efe6d061d5', 'ami-0b898040803850657'])
 
     def test_images_names_from_init_name_map_with_one_image(self):
         test_data = {'AMIID': {'image_name': 'amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2'}}
-        self.assertListEquals(self.get.images_names_from_init_id_map(test_data), ['amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2'])
+        self.assertListEqual(self.get.images_names_from_init_id_map(test_data), ['amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2'])
 
     def test_images_names_from_init_name_map_with_one_image(self):
         test_data = {'AMIID': {'image_name': 'amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2'}, 'AMIID2': {'image_name': 'amzn2-ami-hvm-2.0.20190618-x86_64-gp2'}}
-        self.assertListEquals(self.get.images_names_from_init_name_map(test_data), ['amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2', 'amzn2-ami-hvm-2.0.20190618-x86_64-gp2'])
+        self.assertListEqual(self.get.images_names_from_init_name_map(test_data), ['amzn-ami-hvm-2018.03.0.20190611-x86_64-gp2', 'amzn2-ami-hvm-2.0.20190618-x86_64-gp2'])
 
 if __name__ == '__main__':
     unittest.main()
