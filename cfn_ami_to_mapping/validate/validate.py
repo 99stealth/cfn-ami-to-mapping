4afc4a50b1a77196b40285a976b9c6a6cac57f4f
Roman Banakh <banakh.ri@gmail.com>
2019-09-28 00:26:20 +0300

    Fixed linter

--- cfn_ami_to_mapping/validate/validate.py
+++ cfn_ami_to_mapping/validate/validate.py
@@ -1,12 +1,13 @@
 import re
 
+
 class Validate:
     def images_ids(self, images_ids):
-        ''' The method receives the string with AMI ID and checks if it is 
-        valid. If everything is in order it will return True as a first 
+        ''' The method receives the string with AMI ID and checks if it is
+        valid. If everything is in order it will return True as a first
         argument and None as a second. Otherwise, if AMI ID is an invalid
         method will return False as a first argument and AMI ID as a second '''
-        
+
         for image_id in images_ids:
             image_is_valid = re.match('^ami-(([a-f0-9]{8}$)|([a-f0-9]{17}$))', image_id)
             if not image_is_valid:
