46b05bb1f66f8a15633385316b58e55d761cb37b
Roman Banakh <banakh.ri@gmail.com>
2019-10-01 18:20:32 +0300

    Added validation for aws_access_key_id and aws_secret_access_key

--- cfn_ami_to_mapping/validate/validate.py
+++ cfn_ami_to_mapping/validate/validate.py
@@ -13,3 +13,23 @@ class Validate:
             if not image_is_valid:
                 return False, image_id
         return True, None
+
+    def aws_access_key_id(self, aws_access_key_id):
+        ''' The method receives the string with AWS Access Key ID and checks if
+        it is valid. If everything is in order it will return True otherwise
+        it returns False '''
+
+        if re.match('^AK[A-Z0-9]{18}$', aws_access_key_id):
+            return True
+        else:
+            return False
+
+    def aws_secret_access_key(self, aws_secret_access_key):
+        ''' The method receives the string with AWS Secret Access Key and
+        checks if it is valid. If everything is in order it will return True
+        otherwise it returns False '''
+
+        if re.match('^[A-Za-z0-9+=/]{40}$', aws_secret_access_key):
+            return True
+        else:
+            return False
